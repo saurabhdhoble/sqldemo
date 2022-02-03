bb5a696c007b81bf49ef8040b2288ef16893a43a
Saurabh <saurabh.dhoble@gmail.com>
2022-02-03 00:57:06 -0500

    Added Continent

---
+++ AdventureWorksLT/dbo/Functions/GetAddressContinent.sql
@@ -0,0 +1,9 @@
+﻿CREATE FUNCTION [SalesLT].[GetAddressContinent]
+(
+	@AddressID INT
+)
+RETURNS NVARCHAR(30)
+AS
+BEGIN
+	RETURN (SELECT TOP 1 [ContinentName] FROM [SalesLT].[Address] WHERE [AddressID] = @AddressID);
+END
