13afd212b21f209a3f83742efe9c5b8a1e4a99f7
Saurabh <saurabh.dhoble@gmail.com>
2022-02-09 15:53:22 -0500

    Added complex changes

---
+++ AdventureWorksLT/SalesLT/Stored Procedures/TestProc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [SalesLT].[TestProc]
+	@AddressID INT
+AS
+BEGIN
+	DECLARE @Continent NVARCHAR(30);
+	SET @Continent = [SalesLT].GetAddressContinent(@AddressID);
+
+	SELECT @Continent AS [Continent]
+END
